0b6e481015e9c2ae2f592d7d287f569760a622d3
ShivanandaBasavarajaBiradara <97280766+ShivanandaBasavarajaBiradara@users.noreply.github.com>
2024-09-19 21:35:28 +0530

    Modified and renamed the files

--- ROY_AUTOSYNTHESYS/Grey_Square.v
+++ ROY_AUTOSYNTHESYS/Grey_Square.v
@@ -2,7 +2,10 @@ module Grey_Square (
     output P,H,
     input A,B
 );
-
+/*module (P,H,A,B);
+input A,B;
+output P,H;
+*/
 or Prop (P,A,B);
 xor Gen (H,A,B);
 
--- ROY_AUTOSYNTHESYS/Roy_autosynthesys_tb.v
+++ ROY_AUTOSYNTHESYS/Roy_autosynthesys_tb.v
@@ -4,25 +4,29 @@ module Roy_autosynthesys_tb;
 
 reg [3:0]A,B;
 wire [3:0]X;
+wire Cout;
 integer i;
-Roy_Autosynthesys UUT (.X(X),.A(A),.B(B));
+
+Roy_Autosynthesys UUT (.X(X),.Cout(Cout),.A(A),.B(B));
+
 initial begin
-  A=4'b0000;
-  B=4'b0000;
-  for (i=0;i<10;i++)
-  {
-    A=A+1;
-    B=A+2;
-  }
-  //#10 A=4'b1;
+ /* A = 4'b1000;
+  B = 4'b1000;
+  #10 A = 4'b1111;
+  #10 B = 4'b1111;*/
+A=4'b0000;
+B=4'b0000;
 end
 initial
 $monitor(" X = %b P = %b H = %b X1 = %b",X,UUT.P,UUT.H,UUT.X1);
-
+always #10 A=A+4'b0001;
+always #20 B=B+4'b0001;
 initial
-#100 $finish;
+#2600 $finish;
+
 initial begin
     $dumpfile("Roy_autosynthesys_tb.vcd");
     $dumpvars(0,Roy_autosynthesys_tb); 
 end
+
 endmodule
